0b37b0d3d22a5325e3080eeb38a92a6adcf7cf8a
mahin273 <md.mahin.bd18@gmail.com>
2025-05-31 02:05:01 +0600

    Added basic snake and food in gameArena

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ document.addEventListener('DOMContentLoaded',function(){
     const cellSize = 20;
     let gameStarted = false;
     let score = 0;
-    let food =[{x:300,y:200}];
+    let food ={x:300,y:200};
     let snake=[{x:140,y:200},{x:160,y:200},{x:180,y:200}];
 
+    function drawDiv(x,y,className){
+        const divElement = document.createElement('div');
+        divElement.classList.add(className);
+        divElement.style.top =`${y}px`;
+        divElement.style.left =`${x}px`;
+
+        return divElement;
+    }
+
+    function drawFoodAndSnake(){
+        gameArena.innerHTML='';
+
+        snake.forEach((snakeCell)=>{
+            const snakeElement = drawDiv(snakeCell.x,snakeCell.y,'snake');
+            gameArena.appendChild(snakeElement)
+        })
+
+        const foodElement = drawDiv(food.x,food.y,'food');
+        gameArena.appendChild(foodElement)
+    }
 
     function runGame(){
-        
+        if(!gameStarted){
+            gameStarted = true;
+            drawFoodAndSnake();
+        }
     }
 
     function initiateGame(){
